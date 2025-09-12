7d622d85b0303dc33fa2a5a5e45514e4bf17d976
soniagkalliath <soniageorge111@gmail.com>
2025-09-12 11:05:09 +0530

    wishlist state added in redux store

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
+  const userWishlist = useSelector(state=>state.wishlistReducer)
   return (
     <nav className='flex justify-between p-5 text-lg bg-violet-900 text-white font-bold fixed w-full '>
         <Link to={'/'} className='text-2xl'> <i className="fa-solid fa-truck-fast me-1"></i> <span>Daily Cart</span> </Link>
         <ul className='flex'>
-            <li className='mx-5'><Link to={'/wishlist'}><i className="fa-solid fa-heart text-red-500 me-1"></i>Wishlist<span className='p-1 bg-black  rounded-full ms-1'> 10 </span></Link></li>
+            <li className='mx-5'><Link to={'/wishlist'}><i className="fa-solid fa-heart text-red-500 me-1"></i>Wishlist<span className='p-1 bg-black  rounded-full ms-1'> {userWishlist?.length} </span></Link></li>
             <li className='mx-5'><Link to={'/cart'}><i className="fa-solid fa-cart-shopping text-green-500 me-1"></i>Cart<span className='p-1 bg-black  rounded-full ms-1'>20</span></Link></li>
         </ul>
     </nav>
--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,31 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
+import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToWishlist } from '../redux/slices/wishlistSlice'
 
 const View = () => {
+  const userWishlist = useSelector(state=>state.wishlistReducer)
+  const dispatch = useDispatch()
+  const {id} = useParams()
+  // console.log(id);
+  const [product,setProduct] = useState({})
+  useEffect(()=>{
+    const allProducts = JSON.parse(sessionStorage.getItem("allProducts"))
+    setProduct(allProducts.find(item=>item.id==id))
+  },[])
+  // console.log(product);
+  const handleAddToWishlist = ()=>{
+    //check product is in store wishlist
+    const existingProduct = userWishlist?.find(item=>item.id==product.id)
+    if(existingProduct){
+      alert("Product already exist in your wishlist... Add another!!!")
+    }else{
+      dispatch(addToWishlist(product))
+    }
+  }
+  
   return (
     <>
       <Header/>
       <div style={{paddingTop:'100px'}} className='mx-5'>
-        <div className="grid grid-cols-2 gap-4 items-center">
+        <div className="grid lg:grid-cols-2 gap-4 items-center">
           <div className='flex flex-col justify-center items-center' >
-            <img width={'350px'} height={'250px'} src="https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/thumbnail.webp" alt="product" />
+            <img width={'350px'} height={'250px'} src={product?.thumbnail} alt="product" />
             <div className="flex justify-between mt-5 w-100">
-              <button className="rounded bg-blue-600 p-2 text-white">ADD TO WISHLIST</button>
+              <button onClick={handleAddToWishlist} className="rounded bg-blue-600 p-2 text-white">ADD TO WISHLIST</button>
               <button className="rounded bg-green-600 p-2 text-white">ADD TO CART</button>
             </div>
           </div>
           <div>
-            <h1 className="text-3xl font-bold">Title</h1>
-            <h2 className="text-2xl text-red-500 font-bold">$ 300</h2>
-            <h3 className='text-xl mt-3'><span className='font-bold'>Brand</span> : Essence</h3>
-            <h3 className='text-xl'><span className='font-bold'>Category  </span> : Essence</h3>
-            <h3 className='text-xl'><span className='font-bold'>Description   </span> : Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem et nobis, cum, alias quae debitis nulla cupiditate temporibus officiis impedit deserunt eum perferendis fugit id? Cupiditate saepe temporibus earum cum!</h3>
+            <h1 className="text-3xl font-bold">{product?.title}</h1>
+            <h2 className="text-2xl text-red-500 font-bold">$ {product?.price}</h2>
+            <h3 className='text-xl mt-3'><span className='font-bold'>Brand</span> : {product?.brand}</h3>
+            <h3 className='text-xl'><span className='font-bold'>Category  </span> : {product?.category}</h3>
+            <h3 className='text-xl'><span className='font-bold'>Description   </span> : {product?.description}</h3>
             <h3 className="text-xl font-bold my-5">Client Reviews</h3>
             {/* review tobe duplicated */}
-            <div className="rounded shadow my-3 p-2">
-              <p><span className='font-bold'>client-name</span> : message</p>
-              <p>Rating : 3 <i className="fa-solid fa-star text-yellow-400"></i></p>
-            </div>
+            {
+            product?.reviews?.length>0 ?
+              product?.reviews?.map(item=>(
+                <div className="rounded shadow my-3 p-2">
+                  <p><span className='font-bold'>{item?.reviewerName}</span> : {item?.comment}!</p>
+                  <p>Rating : {item?.rating} <i className="fa-solid fa-star text-yellow-400"></i></p>
+                </div>
+              ))
+              :
+              <p>No reviews yet!!!</p>
+            }
           </div>
         </div>
         
