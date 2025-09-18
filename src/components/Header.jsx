dd1246402ecb302861c229351407efdc322b0efa
soniagkalliath <soniageorge111@gmail.com>
2025-09-18 10:45:52 +0530

    final commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { searchProduct } from '../redux/slices/productSlice'
 
-const Header = () => {
+const Header = ({insideHeader}) => {
+  const dispatch = useDispatch()
   const userWishlist = useSelector(state=>state.wishlistReducer)
   const userCart = useSelector(state=>state.cartReducer)
   return (
     <nav className='flex justify-between p-5 text-lg bg-violet-900 text-white font-bold fixed w-full '>
         <Link to={'/'} className='text-2xl'> <i className="fa-solid fa-truck-fast me-1"></i> <span>Daily Cart</span> </Link>
         <ul className='flex'>
+            {insideHeader && <li className='mx-5'><input onChange={e=>dispatch(searchProduct(e.target.value))} type="text" className="p-1 rounded border w-75" placeholder='Search products here!!!' /></li>}
             <li className='mx-5'><Link to={'/wishlist'}><i className="fa-solid fa-heart text-red-500 me-1"></i>Wishlist<span className='p-1 bg-black  rounded-full ms-1'> {userWishlist?.length} </span></Link></li>
             <li className='mx-5'><Link to={'/cart'}><i className="fa-solid fa-cart-shopping text-green-500 me-1"></i>Cart<span className='p-1 bg-black  rounded-full ms-1'>{userCart?.length}</span></Link></li>
         </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
@@ -8,14 +8,31 @@ const Home = () => {
   const dispatch = useDispatch()
   const {loading,error,allProducts} = useSelector((state)=>state.productReducer)
   // console.log(loading,error,allProducts);
+  const productsPerPage = 8
+  const totalPages = Math.ceil(allProducts?.length/productsPerPage)
+  const [currentPage,setCurrentPage] = useState(1)
+  const currentPageProductsLastIndex = currentPage * productsPerPage
+  const currentPageProductsFirstIndex = currentPageProductsLastIndex - productsPerPage
+  const visibleProductCards = allProducts?.slice(currentPageProductsFirstIndex,currentPageProductsLastIndex)
   
   useEffect(()=>{
     dispatch(fetchAllProducts())
   },[])
 
+  const navigatePrevPage = ()=>{
+    if(currentPage!=1){
+      setCurrentPage(currentPage-1)
+    }
+  }
+
+  const navigateNextPage = ()=>{
+    if(currentPage!=totalPages){
+      setCurrentPage(currentPage+1)
+    }
+  }
   return (
     <>
-      <Header/>
+      <Header insideHeader={true}/>
       <div style={{paddingTop:'100px'}} className='mx-5'>
       {
         loading?
@@ -24,7 +41,7 @@ const Home = () => {
           <div className="grid grid-cols-4 gap-4">
             {
               allProducts?.length>0?
-               allProducts?.map(product=>(
+               visibleProductCards?.map(product=>(
                  <div key={product?.id} className="rounded  p-2 shadow">
                   {/* image */}
                   <img height={'200px'} src={product?.thumbnail} alt="product" />
@@ -39,8 +56,14 @@ const Home = () => {
             :
             <p>Product Not Found</p>
           }
-        </div>
+          </div>
         }
+      </div>
+      {/* pagination */}
+      <div className="text-center mt-20 font-bold text-2xl text-blue-900">
+        <button onClick={navigatePrevPage} className='cursor-pointer'> <i className="fa-solid fa-backward"></i> </button>
+        <span> {currentPage} of {totalPages} </span>
+        <button onClick={navigateNextPage} className='cursor-pointer'> <i className="fa-solid fa-forward"></i> </button>
       </div>
     </>
   )
